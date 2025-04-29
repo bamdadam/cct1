ecff6f7189c97e31875f60cff415077c0f4817e6
Bamdad <kanra252@gmail.com>
2025-04-29 18:08:31 +0330

    fix: ip

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 			panic(err)
 		}
 	})
-	if err := http.ListenAndServe("127.0.0.1:8080", nil); err != nil {
+	if err := http.ListenAndServe("0.0.0.0:8080", nil); err != nil {
 		panic(err)
 	}
 }
